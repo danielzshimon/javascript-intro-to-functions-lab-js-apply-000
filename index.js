fd8352ff11ec36e87541f9dc2ca3a34b5d02b1c6
Daniel Shimon <danielzshimon@gmail.com>
2018-02-22 20:49:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ function sayHiToGrandma(string){
  if(string === "I love you, Grandma."){
    return "I love you, too.";
  } else if(string === string.toUpperCase() ){
-   return 
+   return "YES INDEED!";
+ } else{
+   return
  }
  
 }
