a5198adc1be17c91bca2efe67e0ba9dd9cc0b7d2
Daniel Shimon <danielzshimon@gmail.com>
2018-02-22 20:44:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,24 +11,9 @@ function logShout(string){
 }
 
 function logWhisper(string){
-  
-}
-
-function shout(string){
-  return string.toUpperCase();
-}
-
-function whisper(string){
-  return string.toLowerCase();
-}
-
-function logShout(string){
-  console.log(string.toUpperCase());
-}
-
-function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string){
   if()
+}
