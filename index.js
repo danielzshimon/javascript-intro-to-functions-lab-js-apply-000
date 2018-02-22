bd9590c1da430a3961d191ba9f12dd9c52b09bc8
Daniel Shimon <danielzshimon@gmail.com>
2018-02-22 20:48:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
  if(string === "I love you, Grandma."){
+   return "I love you, too.";
+ } else if(string === string.toUpperCase() ){
    return 
  }
  
